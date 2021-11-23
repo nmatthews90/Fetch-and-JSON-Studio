0e62574bb306692df0829041301ed8f504ef0718
Natasha Matthews <natashamatthews90@gmail.com>
2021-11-22 20:04:23 -0600

    added js to file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const container = document.getElementById("container");
+
+      let astronautInfo = "";
+
+      for (let astronaut of json) {
+        astronautInfo += `<div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName}</h3>
+                <ul>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <li>Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
+            </div>`;
+      }
+
+      container.innerHTML = astronautInfo;
+    });
+});
